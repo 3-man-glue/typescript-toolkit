233e6d4b94a196fdbc467caad49826a09ce9e317
Jinkawin Phongpawarit <jphon@sertiscorp.com>
2021-06-08 04:23:46 +0000

    TSC-18 Http Exception Abstract and Interface

---
+++ src/libs/http-app/exception/http-exception/interfaces.ts
@@ -0,0 +1,20 @@
+import { PlainObject } from 'libs/common-types'
+export interface HttpExceptionObject {
+  status: number
+  message: string
+  code: string
+  input: PlainObject
+  cause: Error
+}
+
+export interface HttpException {
+  status: number
+  message: string
+  code?: string
+  input?: PlainObject
+  cause?: Error
+  toJSON(): HttpExceptionObject
+  toString(): string
+  withInput(value: PlainObject): this
+  withCause(e: Error): this
+}
