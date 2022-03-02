ba4b12fd05c7851b8cf351cdfc3de191ef2050bf
Sarunyou Whangbunyapirat <swhan@sertiscorp.com>
2022-03-02 09:18:25 +0000

    CGW-104 - Sync Common Types

--- src/utils/common-types.ts
+++ src/utils/common-types.ts
@@ -1,5 +1,27 @@
 export type PlainObject = Record<string, unknown>
 
+/**
+ * Mark keys as optional
+ *
+ *  @typeParam T - Type of interface, object or things that has properties
+ *  @typeParam K - Keys, attributes or properties that will be marked as optional
+ *
+ * @example
+ * interface Todo {
+ *   title: string;
+ *   description: string;
+ * }
+ *
+ * function update(fieldsToUpdate: PartialBy<Todo, 'description'>) {
+ *   return { ...fieldsToUpdate }
+ * }
+ *
+ * const todo = update({
+ *   title: 'OKRs Check-ins',
+ * })
+ *
+ */
+export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
 export interface Dimension {
   height: number,
   width: number,
