e5ed37d09b529f1fdffb472ee65c528621834987
Panya Batphantana <fanneioz@yahoo.co.th>
2022-03-06 14:36:56 +0000

    CGW-130 - Ignore type definition file from ESLint and disable explicit any in interface file

--- src/mq/interfaces.ts
+++ src/mq/interfaces.ts
@@ -20,4 +20,5 @@ export interface MessageHandler<T> {
   handle(message: T): Promise<void>
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type MessageHandlerConstructor <T extends MessageDto> = new (...args: any[]) => MessageHandler<T>
